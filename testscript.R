1a9ce873af0e6db264e0de13f589f6e546ae6b62
kicandray <kevin.candray.688@my.csun.edu>
2021-02-01 10:05:22 -0800

    I am adding testscript

---
+++ testscript.R
@@ -0,0 +1,12 @@
+#Please Always Carry Pizza
+  #pull
+    #[git pull] "dowloads any changes to your repository on github
+  #add [git add]
+    #tells to add to repository 
+  #commit [git commit]
+    #saves
+      #requires a message to confirm
+      #-a means commit all, -m adding a message, whatever is in ""
+  #Push. [git push]
+    #uploads changes to github
+a <- 1+1
